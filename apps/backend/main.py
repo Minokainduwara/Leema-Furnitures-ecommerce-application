e3fb3b5a1a273af717c50ab03a16afbffe99fd08
Minokainduwara <minokainduwara@gmail.com>
2026-03-05 00:33:15 +0530

    Feat: Install Cors

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 from fastapi import FastAPI  # type: ignore
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+
 
 # Load your .env.local file first
 load_dotenv(dotenv_path=".env.local")
@@ -14,6 +16,16 @@ print("FIREBASE_CREDENTIALS_PATH:", os.getenv("FIREBASE_CREDENTIALS_PATH"))
 
 # Initialize Firebase
 initialize_firebase()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # your React frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # allow GET, POST, PUT, DELETE etc.
+    allow_headers=["*"],  # allow all headers
+)
+
 
 @app.get("/")
 def root():
